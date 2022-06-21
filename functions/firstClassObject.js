775e4ea56cf59f828501c4fef9b54aab2e41c58f
João Lucas da Costa Fagundes <joaolucasdacf@gmail.com>
2022-06-21 12:05:30 -0300

    Create firstClassObject.js

---
+++ functions/firstClassObject.js
@@ -0,0 +1,22 @@
+const print = function (text_) {
+  console.log(text_)
+}
+
+const obj = {
+  falar: function (name) {
+    console.log(`Meu nome é ${name}`)
+  }
+}
+const array = [print, 1, true, 7.4, false, 'Jorge', obj]
+
+console.log(typeof array[0])
+console.log(typeof array[1])
+console.log(typeof array[2])
+console.log(typeof array[3])
+console.log(typeof array[4])
+console.log(typeof array[5])
+console.log(typeof array[6])
+
+array[0]('Marcos')
+array[0](array[5])
+array[6].falar(array[5])
